4150acb3f5f752bea4b2ec481f0e9b92e1f7e4b4
Mayank Gupta <mayyankgupta09@gmail.com>
2020-09-16 18:37:50 +0100

    Wrote first draft of code for personal blog

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require("express"),
+    app = express()
+
+app.use(express.static(__dirname+"/public"))
+app.set("view engine", "ejs") 
+
+app.get("/", function(req, res){
+    res.render("index")
+})
+
+app.listen(process.env.PORT, process.env.IP)
